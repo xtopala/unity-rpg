b5b2c5e401dc5e98954d97971f0caf4e8fa06f47
xtopala <xtopala@gmail.com>
2020-12-05 20:07:51 +0100

    Namespaces to see some dependencies

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
+
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+}
