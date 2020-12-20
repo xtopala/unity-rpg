e260372f50973cf3b85e3363a2f339c0faa9f5f7
xtopala <xtopala@gmail.com>
2020-12-20 21:52:37 +0100

    Finished scene management

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            print("OY");
+            yield return FadeIn(1f);
+            print("G");
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+
+        }
+    }
+}
