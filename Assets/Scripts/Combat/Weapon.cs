9f6c6606dd59b0425a393b818c85b7b2167f9b39
xtopala <xtopala@gmail.com>
2021-04-14 15:03:07 +0200

    Boom microphone audio listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-            print("Weapon hit " + gameObject.name);
+            onHit.Invoke();
         }
     }
 }
